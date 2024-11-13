ac6c6e302b31dff0f908c23fc67f07b1e6b085f7
Martin133458 <perdunstaryj76@gmail.com>
2024-11-13 20:48:03 +0500

    Add files via upload

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Header.scss"
+
+const Header = () => {
+  return (
+    <>
+    <header className="header">
+      <div className="container">
+        <nav className="nav">
+          <a href="#hero1">Section 1</a>
+          <a href="#hero2">Section 2</a>
+          <a href="#hero3">Section 3</a>
+          <a href="#hero4">Section 4</a>
+          <a href="#hero5">Section 5</a>
+         
+        </nav>
+      </div>
+    </header>
+    
+    </>
+  )
+}
+
+export default Header
